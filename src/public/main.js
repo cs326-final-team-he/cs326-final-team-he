90ee3f983026dc53dd92b28470a272cca73aa6b4
jmachiaverna <jmachiaverna@umass.edu>
2022-12-03 14:32:13 -0500

    seeing how things work

--- src/public/main.js
+++ src/public/main.js
@@ -154,7 +154,7 @@ async function post_chirp(chirp_json) {
         favorite.classList.add('like_button');
         favorite.innerText = 'favorite_border';
         favorite.addEventListener('click', () => {
-            console.log(chirp_json.like_count)
+            console.log(chirp_json.like_count);
         });
         const publish = document.createElement('span');
         publish.classList.add('material-icons');
