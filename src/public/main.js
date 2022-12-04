54a15107bcddf72ffccbb13e564facb1e228fdfb
jmachiaverna <jmachiaverna@umass.edu>
2022-12-04 11:25:37 -0500

    seeing how things work

--- src/public/main.js
+++ src/public/main.js
@@ -164,6 +164,7 @@ async function post_chirp(chirp_json) {
         favorite.classList.add('like_button');
         favorite.innerText = 'favorite_border';
         const likedPost = await fetch(`https://music-matcher-326.herokuapp.com/likedChirps/${profile_json.user_id}/${chirp_json.chirp_id}`);
+        console.log(likedPost);
         if (likedPost.length == 0) {
             favorite.style.color = 'black';
         }
