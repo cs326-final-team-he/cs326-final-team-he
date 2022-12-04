509e97904e376f1a23d774e3c12ac82041b31356
jmachiaverna <jmachiaverna@umass.edu>
2022-12-04 11:57:03 -0500

    typo fix

--- src/public/main.js
+++ src/public/main.js
@@ -165,8 +165,7 @@ async function post_chirp(chirp_json) {
         favorite.innerText = 'favorite_border';
         const response = await fetch(`https://music-matcher-326.herokuapp.com/likedChirps/${profile_json.user_id}/${chirp_json.chirp_id}`);
         const likedPost = response.json();
-        console.log(likedPost);
-        if (likedPost) {
+        if (likedPost.PromiseResult) {
             favorite.style.color = 'black';
         }
         favorite.addEventListener('click', async () => {
