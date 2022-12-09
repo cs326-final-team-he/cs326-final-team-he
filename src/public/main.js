fe52c93ae82d8cfb503dddf6fee0f70b4d563344
halfu <nickchafy@gmail.com>
2022-12-09 00:39:04 -0500

    debuggin

--- src/public/main.js
+++ src/public/main.js
@@ -148,7 +148,7 @@ async function post_chirp(chirp_json) {
 
 async function search() {
     const search = document.getElementById('search').value;
-    const response = await fetch(`https://music-matcher-326.herokuapp.com/search/?search=${search}`);
+    const response = await fetch(`https://music-matcher-326.herokuapp.com/search?search=${search}`);
     if (response.ok && response.status !== 404) {
         const rows = await response.json();
         const results = document.getElementById('results');
