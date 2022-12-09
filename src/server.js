0d97370de21ca609b8e97df12c150b6b9232be90
halfu <nickchafy@gmail.com>
2022-12-08 20:24:43 -0500

    debugging

--- src/server.js
+++ src/server.js
@@ -30,7 +30,8 @@ const strategy = new LocalStrategy(
 	},
 
     async (user_id, password, done) => {
-        const userExists = await findUser(user_id);
+        // const userExists = await findUser(user_id);
+        const userExists = true;
         if (!userExists) {
             // no such user
             await new Promise((r) => setTimeout(r, 2000)); // two second delay
